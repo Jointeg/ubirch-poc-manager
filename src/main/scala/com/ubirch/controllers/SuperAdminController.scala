bfc29ad1f82379c04d9cf127fb2d08430fce8941
Wojtek Urbanski <wojciech.urbanski@softwaremill.com>
2021-03-23 12:28:32 +0100

    UNG-1642 - Test base for controllers

--- src/main/scala/com/ubirch/controllers/SuperAdminController.scala
+++ src/main/scala/com/ubirch/controllers/SuperAdminController.scala
@@ -3,26 +3,30 @@ package com.ubirch.controllers
 import com.google.inject.Singleton
 import com.typesafe.config.Config
 import com.ubirch.ConfPaths.GenericConfPaths
-import com.ubirch.controllers.concerns.ControllerBase
+import com.ubirch.controllers.concerns.{ControllerBase, KeycloakBearerAuthStrategy, KeycloakBearerAuthenticationSupport, Token}
 import com.ubirch.models.NOK
+import com.ubirch.services.jwt.{PublicKeyPoolService, TokenVerificationService}
 import io.prometheus.client.Counter
 import monix.eval.Task
 import monix.execution.Scheduler
 import org.json4s.Formats
-import org.scalatra.{NotFound, Ok}
 import org.scalatra.swagger.{Swagger, SwaggerSupportSyntax}
+import org.scalatra.{NotFound, Ok, ScalatraBase}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class SuperAdminController @Inject()(config: Config, val swagger: Swagger, jFormats: Formats)(implicit val executor: ExecutionContext, scheduler: Scheduler)
-  extends ControllerBase {
+class SuperAdminController @Inject()(config: Config, val swagger: Swagger, jFormats: Formats, publicKeyPoolService: PublicKeyPoolService,
+                                     tokenVerificationService: TokenVerificationService)(implicit val executor: ExecutionContext, scheduler: Scheduler)
+  extends ControllerBase with KeycloakBearerAuthenticationSupport {
   override protected val applicationDescription: String = "Super Admin controller"
 
   override protected implicit def jsonFormats: Formats = jFormats
 
   override val service: String = config.getString(GenericConfPaths.NAME)
+
+  override protected def createStrategy(app: ScalatraBase): KeycloakBearerAuthStrategy = new KeycloakBearerAuthStrategy(app, tokenVerificationService, publicKeyPoolService)
 
   override val successCounter: Counter =
     Counter.build()
@@ -44,22 +48,29 @@ class SuperAdminController @Inject()(config: Config, val swagger: Swagger, jForm
       tags "Test")
 
   get("/initialTest", operation(getSimpleCheck)) {
-    asyncResult("initialTest") { _ =>
-      _ =>
-        Task(test())
-    }
-  }
-
-  notFound {
-    asyncResult("not_found") { _ => _ =>
-      Task {
-        logger.info("controller=SuperAdminController route_not_found={} query_string={}", requestPath, request.getQueryString)
-        NotFound(NOK.noRouteFound(requestPath + " might exist in another universe"))
+    authenticated() { token =>
+      asyncResult("initialTest") { _ =>
+        _ =>
+          Task(test(token))
       }
     }
   }
 
-  private def test() = {
-    Ok("Test successful")
+  notFound {
+    asyncResult("not_found") { _ =>
+      _ =>
+        Task {
+          logger.info("controller=SuperAdminController route_not_found={} query_string={}", requestPath, request.getQueryString)
+          NotFound(NOK.noRouteFound(requestPath + " might exist in another universe"))
+        }
+    }
+  }
+
+  before() {
+    contentType = "application/json"
+  }
+
+  private def test(token: Token) = {
+    Ok(s"Test successful for ${token.name}")
   }
 }
---
+++ src/test/scala/com/ubirch/InjectorHelperImpl.scala
@@ -0,0 +1,262 @@
+package com.ubirch
+
+import com.google.inject.binder.ScopedBindingBuilder
+import com.typesafe.config.Config
+import com.ubirch.crypto.utils.Curve
+import com.ubirch.crypto.{GeneratorKeyFactory, PrivKey}
+import com.ubirch.services.jwt.{DefaultPublicKeyPoolService, PublicKeyDiscoveryService, PublicKeyPoolService, TokenCreationService}
+import monix.eval.Task
+
+import java.security.Key
+import javax.inject.{Inject, Provider, Singleton}
+
+@Singleton
+class FakeDefaultPublicKeyPoolService @Inject()(privKey: PrivKey, config: Config, publicKeyDiscoveryService: PublicKeyDiscoveryService)
+  extends DefaultPublicKeyPoolService(config, publicKeyDiscoveryService) {
+
+  override def getKeyFromDiscoveryService(kid: String): Task[Option[Key]] = Task {
+    kid match {
+      case "6dMHOUfu7v6howP2WH5bkp-j9UgUYdyEQbWJp8cb8IY" => Some(privKey.getPublicKey)
+      case _ => throw new Exception("Check my kid!")
+    }
+  }
+}
+
+@Singleton
+class KeyPairProvider extends Provider[PrivKey] {
+  val privKey: PrivKey = GeneratorKeyFactory.getPrivKey(Curve.PRIME256V1)
+
+  override def get(): PrivKey = privKey
+}
+
+case class FakeToken(value: String) {
+  def prepare: String = "bearer " + value
+}
+
+object FakeToken {
+
+  val header: String =
+    """
+      |{
+      |  "alg": "ES256",
+      |  "typ": "JWT",
+      |  "kid": "6dMHOUfu7v6howP2WH5bkp-j9UgUYdyEQbWJp8cb8IY"
+      |}""".stripMargin
+
+  val admin: String =
+    """
+      |{
+      |  "exp": 1718338181,
+      |  "iat": 1604336381,
+      |  "jti": "2fb1c61d-2113-4b8e-9432-97c28c697b98",
+      |  "iss": "https://id.dev.ubirch.com/auth/realms/ubirch-default-realm",
+      |  "aud": "account",
+      |  "sub": "963995ed-ce12-4ea5-89dc-b181701d1d7b",
+      |  "typ": "Bearer",
+      |  "azp": "ubirch-2.0-user-access",
+      |  "session_state": "f334122a-4693-4826-a2c0-546391886eda",
+      |  "acr": "1",
+      |  "allowed-origins": [
+      |    "http://localhost:9101",
+      |    "https://console.dev.ubirch.com"
+      |  ],
+      |  "realm_access": {
+      |    "roles": [
+      |      "offline_access",
+      |      "ADMIN",
+      |      "uma_authorization",
+      |      "USER"
+      |    ]
+      |  },
+      |  "resource_access": {
+      |    "account": {
+      |      "roles": [
+      |        "manage-account",
+      |        "manage-account-links",
+      |        "view-profile"
+      |      ]
+      |    }
+      |  },
+      |  "scope": "fav_color profile email",
+      |  "email_verified": true,
+      |  "fav_fruit": [
+      |    "/OWN_DEVICES_carlos.sanchez@ubirch.com"
+      |  ],
+      |  "name": "Carlos Sanchez",
+      |  "preferred_username": "carlos.sanchez@ubirch.com",
+      |  "given_name": "Carlos",
+      |  "family_name": "Sanchez",
+      |  "email": "carlos.sanchez@ubirch.com"
+      |}""".stripMargin
+
+  val user: String =
+    """
+      |{
+      |  "exp": 1718338181,
+      |  "iat": 1604336381,
+      |  "jti": "2fb1c61d-2113-4b8e-9432-97c28c697b98",
+      |  "iss": "https://id.dev.ubirch.com/auth/realms/ubirch-default-realm",
+      |  "aud": "account",
+      |  "sub": "963995ed-ce12-4ea5-89dc-b181701d1d7b",
+      |  "typ": "Bearer",
+      |  "azp": "ubirch-2.0-user-access",
+      |  "session_state": "f334122a-4693-4826-a2c0-546391886eda",
+      |  "acr": "1",
+      |  "allowed-origins": [
+      |    "http://localhost:9101",
+      |    "https://console.dev.ubirch.com"
+      |  ],
+      |  "realm_access": {
+      |    "roles": [
+      |      "offline_access",
+      |      "uma_authorization",
+      |      "USER",
+      |      "certification-vaccination",
+      |      "vaccination-center-altoetting"
+      |    ]
+      |  },
+      |  "resource_access": {
+      |    "account": {
+      |      "roles": [
+      |        "manage-account",
+      |        "manage-account-links",
+      |        "view-profile"
+      |      ]
+      |    }
+      |  },
+      |  "scope": "fav_color profile email",
+      |  "email_verified": true,
+      |  "fav_fruit": [
+      |    "/OWN_DEVICES_carlos.sanchez@ubirch.com"
+      |  ],
+      |  "name": "Carlos Sanchez",
+      |  "preferred_username": "carlos.sanchez@ubirch.com",
+      |  "given_name": "Carlos",
+      |  "family_name": "Sanchez",
+      |  "email": "carlos.sanchez@ubirch.com"
+      |}""".stripMargin
+
+  val userNoPrincipal: String =
+    """
+      |{
+      |  "exp": 1718338181,
+      |  "iat": 1604336381,
+      |  "jti": "2fb1c61d-2113-4b8e-9432-97c28c697b98",
+      |  "iss": "https://id.dev.ubirch.com/auth/realms/ubirch-default-realm",
+      |  "aud": "account",
+      |  "sub": "963995ed-ce12-4ea5-89dc-b181701d1d7b",
+      |  "typ": "Bearer",
+      |  "azp": "ubirch-2.0-user-access",
+      |  "session_state": "f334122a-4693-4826-a2c0-546391886eda",
+      |  "acr": "1",
+      |  "allowed-origins": [
+      |    "http://localhost:9101",
+      |    "https://console.dev.ubirch.com"
+      |  ],
+      |  "realm_access": {
+      |    "roles": [
+      |      "offline_access",
+      |      "uma_authorization",
+      |      "USER",
+      |      "vaccination-center-altoetting",
+      |      "vaccination-center-berlin"
+      |    ]
+      |  },
+      |  "resource_access": {
+      |    "account": {
+      |      "roles": [
+      |        "manage-account",
+      |        "manage-account-links",
+      |        "view-profile"
+      |      ]
+      |    }
+      |  },
+      |  "scope": "fav_color profile email",
+      |  "email_verified": true,
+      |  "fav_fruit": [
+      |    "/OWN_DEVICES_carlos.sanchez@ubirch.com"
+      |  ],
+      |  "name": "Carlos Sanchez",
+      |  "preferred_username": "carlos.sanchez@ubirch.com",
+      |  "given_name": "Carlos",
+      |  "family_name": "Sanchez",
+      |  "email": "carlos.sanchez@ubirch.com"
+      |}""".stripMargin
+
+  val userWithDoubleRoles: String =
+    """
+      |{
+      |  "exp": 1718338181,
+      |  "iat": 1604336381,
+      |  "jti": "2fb1c61d-2113-4b8e-9432-97c28c697b98",
+      |  "iss": "https://id.dev.ubirch.com/auth/realms/ubirch-default-realm",
+      |  "aud": "account",
+      |  "sub": "963995ed-ce12-4ea5-89dc-b181701d1d7b",
+      |  "typ": "Bearer",
+      |  "azp": "ubirch-2.0-user-access",
+      |  "session_state": "f334122a-4693-4826-a2c0-546391886eda",
+      |  "acr": "1",
+      |  "allowed-origins": [
+      |    "http://localhost:9101",
+      |    "https://console.dev.ubirch.com"
+      |  ],
+      |  "realm_access": {
+      |    "roles": [
+      |      "offline_access",
+      |      "uma_authorization",
+      |      "USER",
+      |      "certification-vaccination",
+      |      "vaccination-center-altoetting",
+      |      "vaccination-center-berlin"
+      |    ]
+      |  },
+      |  "resource_access": {
+      |    "account": {
+      |      "roles": [
+      |        "manage-account",
+      |        "manage-account-links",
+      |        "view-profile"
+      |      ]
+      |    }
+      |  },
+      |  "scope": "fav_color profile email",
+      |  "email_verified": true,
+      |  "fav_fruit": [
+      |    "/OWN_DEVICES_carlos.sanchez@ubirch.com"
+      |  ],
+      |  "name": "Carlos Sanchez",
+      |  "preferred_username": "carlos.sanchez@ubirch.com",
+      |  "given_name": "Carlos",
+      |  "family_name": "Sanchez",
+      |  "email": "carlos.sanchez@ubirch.com"
+      |}""".stripMargin
+
+}
+
+@Singleton
+class FakeTokenCreator @Inject()(privKey: PrivKey, tokenCreationService: TokenCreationService) {
+
+  def fakeToken(header: String, token: String): FakeToken = {
+    FakeToken(
+      tokenCreationService.encode(header, token, privKey)
+        .getOrElse(throw new Exception("Error Creating Token"))
+    )
+  }
+
+  val user: FakeToken = fakeToken(FakeToken.header, FakeToken.user)
+  val userWithDoubleRoles: FakeToken = fakeToken(FakeToken.header, FakeToken.userWithDoubleRoles)
+  val userNoPrincipal: FakeToken = fakeToken(FakeToken.header, FakeToken.userNoPrincipal)
+  val admin: FakeToken = fakeToken(FakeToken.header, FakeToken.admin)
+
+}
+
+class InjectorHelperImpl() extends InjectorHelper(List(new Binder {
+  override def PublicKeyPoolService: ScopedBindingBuilder = {
+    bind(classOf[PublicKeyPoolService]).to(classOf[FakeDefaultPublicKeyPoolService])
+  }
+
+  override def configure(): Unit = {
+    super.configure()
+    bind(classOf[PrivKey]).toProvider(classOf[KeyPairProvider])
+  }
+}))
