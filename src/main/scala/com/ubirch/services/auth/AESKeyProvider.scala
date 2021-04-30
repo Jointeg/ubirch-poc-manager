97c172c0bf51199bee55f87c640538d880352925
Wojtek Urbanski <wojciech.urbanski@softwaremill.com>
2021-04-30 13:17:20 +0200

    UNG-2092 - accept secretKey as Base64

--- src/main/scala/com/ubirch/services/auth/AESKeyProvider.scala
+++ src/main/scala/com/ubirch/services/auth/AESKeyProvider.scala
@@ -3,9 +3,9 @@ package com.ubirch.services.auth
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.ConfPaths.AESEncryptionPaths
+import com.ubirch.models.auth.Base64String
 import monix.eval.Task
 
-import java.nio.charset.StandardCharsets
 import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
 import javax.inject.Inject
@@ -15,8 +15,8 @@ trait AESKeyProvider {
 }
 
 class ConfigKeyProvider @Inject() (config: Config) extends AESKeyProvider with LazyLogging {
-  private val key = config.getString(AESEncryptionPaths.SECRET_KEY).getBytes(StandardCharsets.UTF_8)
+  private val key = Base64String(config.getString(AESEncryptionPaths.SECRET_KEY))
 
   override def getAESKey: Task[SecretKey] =
-    Task(new SecretKeySpec(key, "AES"))
+    Task(new SecretKeySpec(key.decode, "AES"))
 }
