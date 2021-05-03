02bb278838f6c7c426879a5df7d39acf88daf14f
Rueger <lotta.rueger@ubirch.com>
2021-05-03 11:15:38 +0200

    deletion of log that was only added for short term debugging

--- src/main/scala/com/ubirch/services/auth/AESKeyProvider.scala
+++ src/main/scala/com/ubirch/services/auth/AESKeyProvider.scala
@@ -17,8 +17,6 @@ trait AESKeyProvider {
 class ConfigKeyProvider @Inject() (config: Config) extends AESKeyProvider with LazyLogging {
   private val key = Base64String(config.getString(AESEncryptionPaths.SECRET_KEY))
 
-  override def getAESKey: Task[SecretKey] = {
-    println(s"AES key base64 encoded: ${key.value}")
+  override def getAESKey: Task[SecretKey] =
     Task(new SecretKeySpec(key.decode, "AES"))
-  }
 }
