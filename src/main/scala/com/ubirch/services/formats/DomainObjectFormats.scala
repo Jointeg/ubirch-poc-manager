58dc7c11d2dd29a981b697e6549ae7dbbb0dcaca
Wojtek Urbanski <wojciech.urbanski@softwaremill.com>
2021-05-05 13:54:06 +0200

    UNG-2088-Add ClientCert to CreateTenant request and store it in DB

--- src/main/scala/com/ubirch/services/formats/DomainObjectFormats.scala
+++ src/main/scala/com/ubirch/services/formats/DomainObjectFormats.scala
@@ -1,20 +1,28 @@
 package com.ubirch.services.formats
+import com.ubirch.models.auth.Base64String
 import com.ubirch.models.tenant._
+import org.apache.commons.codec.binary.Base64
 import org.json4s.JsonAST.JString
 import org.json4s.{ CustomSerializer, MappingException }
 
 object DomainObjectFormats {
-  val all = List(
-    createStringFormat(UsageType.unsafeFromString)(UsageType.toStringFormat)
-  )
 
-  private def createStringFormat[A: Manifest](decode: String => A)(encode: A => String) = {
+  private val usageTypeFormat: CustomSerializer[UsageType] =
+    createStringFormat(UsageType.unsafeFromString, _.nonEmpty)(UsageType.toStringFormat)
+  private val clientCertFormat: CustomSerializer[ClientCert] =
+    createStringFormat(string => ClientCert(Base64String(string)), string => Base64.isBase64(string))(_.value.value)
+
+  val all = List(usageTypeFormat, clientCertFormat)
+
+  private def createStringFormat[A: Manifest](
+    decode: String => A,
+    validation: String => Boolean)(encode: A => String) = {
     val Class = implicitly[Manifest[A]].runtimeClass
     new CustomSerializer[A](_ =>
       (
         {
-          case JString(value) if value.nonEmpty => decode(value)
-          case JString(_)                       => throw new MappingException("Can't convert empty string to " + Class)
+          case JString(value) if validation(value) => decode(value)
+          case JString(_)                          => throw new MappingException("Can't convert value to " + Class)
         },
         {
           case a: A => JString(encode(a))
