a4f5094aada76373ba9d422c21c43d32e592f2d3
Hiroki6 <hirokifujino0108@gmail.com>
2021-05-14 13:33:42 +0200

    UNG-2283 change DeviceHelper

--- src/test/scala/com/ubirch/PocConfigTest.scala
+++ src/test/scala/com/ubirch/PocConfigTest.scala
@@ -5,7 +5,7 @@ class PocConfigTest extends UnitTestBase {
     "success to load as Map[String, String]" in {
       withInjector { injector =>
         val pocConfig = injector.get[PocConfig]
-        val expected = Map("group-name1" -> "uuid1", "group-name2" -> "uuid2")
+        val expected = Map("group-name1" -> "uuid1", "group-name2" -> "uuid2", "data-schema-id" -> "c618b7cf-d798-49c8-adb1-c91a122904f6")
         pocConfig.dataSchemaGroupMap shouldBe expected
       }
     }
