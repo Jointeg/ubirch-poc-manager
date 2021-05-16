a25e7e2b4105caaa3a8af3638dc96163878157fd
Rueger <lotta.rueger@ubirch.com>
2021-05-16 21:59:06 +0200

    fixing to always use the latest timestamp for lastUpdated when storing object to database

--- src/main/scala/com/ubirch/models/poc/Updated.scala
+++ src/main/scala/com/ubirch/models/poc/Updated.scala
@@ -9,7 +9,7 @@ case class Updated(dateTime: DateTime) extends Embedded
 object Updated {
   private val formatter = new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").toFormatter
   implicit val encodeUpdated: MappedEncoding[Updated, String] =
-    MappedEncoding[Updated, String](u => u.dateTime.toString(formatter))
+    MappedEncoding[Updated, String](_ => DateTime.now().toString(formatter))
 
   implicit val decodeUpdated: MappedEncoding[String, Updated] =
     MappedEncoding[String, Updated](str => Updated(DateTime.parse(str, formatter)))
