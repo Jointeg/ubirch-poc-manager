2fe67e8d331cd52acf605184a5b737872ebc3ede
Hiroki6 <hirokifujino0108@gmail.com>
2021-05-28 22:15:22 +0200

    fix UnitTest

--- src/test/scala/com/ubirch/services/poc/parsers/PocCsvParserTest.scala
+++ src/test/scala/com/ubirch/services/poc/parsers/PocCsvParserTest.scala
@@ -50,7 +50,7 @@ class PocCsvParserTest extends TestBase {
     "throw a HeaderCsvException if row length is not enough" in {
       val result = pocCsvParser.parseList(validHeaderButNotEnoughRowsPocOnlyCsv(pocId), tenant).runSyncUnsafe()
       assert(result.size == 1)
-      assert(result.head.left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;0187-738786782;TRUE;;FALSE;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;{"vaccines":["vaccine1", "vaccine2"]};the number of column 20 is invalid. should be 21.""")
+      assert(result.head.left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;+4974339296;TRUE;;FALSE;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;{"vaccines":["vaccine1", "vaccine2"]};the number of column 20 is invalid. should be 21.""")
     }
 
     "return invalid csvRows with errorMsg and validCsvRow as Poc" in {
@@ -59,14 +59,14 @@ class PocCsvParserTest extends TestBase {
       assert(result.size == 6)
       assert(result.head.isRight)
       assert(result(
-        1).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;0187-738786782;FALSE;;Xfalse;certification-vaccination;Musterfrau;Frau;frau.musterfraumail.de;0187-738786782;{"vaccines":["vaccine1", "vaccine2"]};column client_cert* must be either 'TRUE' or 'FALSE',column manager_email* must contain a proper mail address""")
+        1).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;+4974339296;FALSE;;Xfalse;certification-vaccination;Musterfrau;Frau;frau.musterfraumail.de;+4974339296;{"vaccines":["vaccine1", "vaccine2"]};column client_cert* must be either 'TRUE' or 'FALSE',column manager_email* must contain a proper mail address""")
       assert(result(
-        2).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;;;;;Wunschkreis;Wunschland;Deutschland;0187-738786782;false;;FALSE;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;0187-738786782;{"vaccines":["vaccine1", "vaccine2"]};column street_number* cannot be empty,column zipcode* must have the length of 5 digits,column city* cannot be empty""")
+        2).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;;;;;Wunschkreis;Wunschland;Deutschland;+4974339296;false;;FALSE;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;+4974339296;{"vaccines":["vaccine1", "vaccine2"]};column street_number* cannot be empty,column zipcode* must have the length of 5 digits,column city* cannot be empty""")
       assert(result(
-        3).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;0187-738786782;false;;FALSE;certification;Musterfrau;Frau;frau.musterfrau@mail.de;0187-738786782;{"vaccines":["vaccine1", "vaccine2"]};column data_schema_id* must contain a valid value from this map Map(dataSchemaGroups -> xxx, certification-vaccination -> yyy)""")
+        3).left.get == s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;+4974339296;false;;FALSE;certification;Musterfrau;Frau;frau.musterfrau@mail.de;+4974339296;{"vaccines":["vaccine1", "vaccine2"]};column data_schema_id* must contain a valid value from this map Map(dataSchemaGroups -> xxx, certification-vaccination -> yyy)""")
       assert(result(4).isRight)
       assert(result.last.left.get ==
-        s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;0187-738786782;;false;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;0187-738786782;{"vaccines":["vaccine1", "vaccine2"]};the number of column 20 is invalid. should be 21.""")
+        s"""${pocId.toString};ub_vac_app;pocName;pocStreet;101;;12636;Wunschstadt;Wunschkreis;Wunschland;Deutschland;+4974339296;;false;certification-vaccination;Musterfrau;Frau;frau.musterfrau@mail.de;+4974339296;{"vaccines":["vaccine1", "vaccine2"]};the number of column 20 is invalid. should be 21.""")
     }
   }
 }
