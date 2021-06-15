767090b9b03663f4234ab98fcb8c4703af87bdf7
Wojtek Urbanski <wojciech.urbanski@softwaremill.com>
2021-06-15 17:25:16 +0200

    UNG-2551 - Adding to readiness check status of creator loops

--- src/main/scala/com/ubirch/services/poc/PocAdminCreationLoop.scala
+++ src/main/scala/com/ubirch/services/poc/PocAdminCreationLoop.scala
@@ -1,12 +1,17 @@
 package com.ubirch.services.poc
 
+import cats.effect.ExitCase
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.ConfPaths.ServicesConfPaths.POC_CREATION_INTERVAL
+import com.ubirch.models.common._
+import monix.eval.Task
+import monix.execution.atomic.AtomicAny
 import monix.reactive.Observable
 
 import javax.inject.Inject
 import scala.concurrent.duration.DurationInt
+import scala.util.control.NonFatal
 
 trait PocAdminCreationLoop {
   def startPocAdminCreationLoop[T](operation: PocAdminCreationResult => Observable[T]): Observable[T]
@@ -20,17 +25,27 @@ class PocAdminCreationLoopImpl @Inject() (conf: Config, pocAdminCreator: PocAdmi
   private val startPocAdminCreation: Observable[PocAdminCreationResult] = {
     for {
       _ <- Observable.intervalWithFixedDelay(pocAdminCreatorInterval.seconds)
+      _ <- Observable.fromTask(Task(PocAdminCreationLoop.loopState.set(Running)))
       result <- Observable.fromTask(pocAdminCreator.createPocAdmins())
     } yield result
   }
 
   private def retryWithDelay[A](source: Observable[A]): Observable[A] = {
-    source.onErrorHandleWith { ex =>
-      logger.error("some unexpected error occurred during poc admin creation", ex)
-      retryWithDelay(source).delayExecution(pocAdminCreatorInterval.seconds)
+    source.onErrorHandleWith {
+      case NonFatal(ex) =>
+        logger.error("some unexpected error occurred during poc admin creation", ex)
+        retryWithDelay(source).delayExecution(pocAdminCreatorInterval.seconds)
     }
   }
 
   override def startPocAdminCreationLoop[T](operation: PocAdminCreationResult => Observable[T]): Observable[T] =
-    retryWithDelay(startPocAdminCreation.flatMap(operation))
+    retryWithDelay(startPocAdminCreation.flatMap(operation)).guaranteeCase {
+      case ExitCase.Canceled  => Task(PocAdminCreationLoop.loopState.set(Cancelled))
+      case ExitCase.Error(_)  => Task(PocAdminCreationLoop.loopState.set(ErrorTerminated))
+      case ExitCase.Completed => Task(PocAdminCreationLoop.loopState.set(Completed))
+    }
+}
+
+object PocAdminCreationLoop {
+  val loopState: AtomicAny[LoopState] = AtomicAny(Starting)
 }
--- src/main/scala/com/ubirch/services/poc/PocEmployeeCreationLoop.scala
+++ src/main/scala/com/ubirch/services/poc/PocEmployeeCreationLoop.scala
@@ -1,12 +1,17 @@
 package com.ubirch.services.poc
 
+import cats.effect.ExitCase
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.ConfPaths.ServicesConfPaths.POC_CREATION_INTERVAL
+import com.ubirch.models.common._
+import monix.eval.Task
+import monix.execution.atomic.AtomicAny
 import monix.reactive.Observable
 
 import javax.inject.Inject
 import scala.concurrent.duration.DurationInt
+import scala.util.control.NonFatal
 
 trait PocEmployeeCreationLoop {
   def startPocEmployeeCreationLoop[T](operation: PocEmployeeCreationResult => Observable[T]): Observable[T]
@@ -20,17 +25,27 @@ class PocEmployeeCreationLoopImpl @Inject() (conf: Config, employeeCreator: PocE
   private val startPocEmployeeCreation: Observable[PocEmployeeCreationResult] = {
     for {
       _ <- Observable.intervalWithFixedDelay(pocEmployeeCreatorInterval.seconds)
+      _ <- Observable.fromTask(Task(PocEmployeeCreationLoop.loopState.set(Running)))
       result <- Observable.fromTask(employeeCreator.createPocEmployees())
     } yield result
   }
 
   private def retryWithDelay[A](source: Observable[A]): Observable[A] = {
-    source.onErrorHandleWith { ex =>
-      logger.error("some unexpected error occurred during poc employee creation", ex)
-      retryWithDelay(source).delayExecution(pocEmployeeCreatorInterval.seconds)
+    source.onErrorHandleWith {
+      case NonFatal(ex) =>
+        logger.error("some unexpected error occurred during poc employee creation", ex)
+        retryWithDelay(source).delayExecution(pocEmployeeCreatorInterval.seconds)
     }
   }
 
   override def startPocEmployeeCreationLoop[T](operation: PocEmployeeCreationResult => Observable[T]): Observable[T] =
-    retryWithDelay(startPocEmployeeCreation.flatMap(operation))
+    retryWithDelay(startPocEmployeeCreation.flatMap(operation)).guaranteeCase {
+      case ExitCase.Canceled  => Task(PocEmployeeCreationLoop.loopState.set(Cancelled))
+      case ExitCase.Error(_)  => Task(PocEmployeeCreationLoop.loopState.set(ErrorTerminated))
+      case ExitCase.Completed => Task(PocEmployeeCreationLoop.loopState.set(Completed))
+    }
+}
+
+object PocEmployeeCreationLoop {
+  val loopState: AtomicAny[LoopState] = AtomicAny(Starting)
 }
